ee1536f24d240cbfc5d13591553d5f94d9ed815c
soo <hsw2657@gmail.com>
2022-09-14 12:23:02 +0900

    .meta delete
    이클립스 설정 파일 삭제

--- JavaProject/FirstJava/src/chapter02/Person.java
+++ JavaProject/FirstJava/src/chapter02/Person.java
@@ -3,8 +3,13 @@ package chapter02;
 public class Person {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		// ① 회원이름을 저장하는 변수
+		String memberName = null;
+		// ② 회원 전화번호 ( 010-0000-0000 )를 저장하는 변수
+		String phoneNumber = null;
+		// ③ 회원 주민등록번호 ( 000000-0000000 또는 0000000000000 )를 저장하는 변수
+		String personNumber = null;
 	}
 
 }
