cfd85d71d590ecd987ef184ae558edcb9d27257d
soo <hsw2657@gmail.com>
2022-09-14 12:13:30 +0900

    Initial commit

---
+++ JavaProject/FirstJava/src/first/FirstJava.java
@@ -0,0 +1,19 @@
+package first;
+
+
+
+public class FirstJava {
+
+	//변수
+	//메소드
+	
+	//main 메소드가 클래스의 시작포인트
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 
+		System.out.println("Hello Java");
+		System.out.println(20);	
+		
+	}
+
+}
