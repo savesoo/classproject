f1c9b0d2ccd2b914cda2f8bcf9a29890823fcbbb
soo <hsw2657@gmail.com>
2022-10-14 17:37:58 +0900

    수정
    .

--- sql/22021014MYSQL.sql
+++ sql/22021014MYSQL.sql
@@ -7,24 +7,33 @@ select *
 from emp e inner join dept d
 on e.deptno = d.deptno;
 
+-- outer join : 다른 테이블에 없는 데이터 출력
 select e1.ename, e2.ename
-from emp e1 left outer join emp e2 -- 표시할 대상 개체: 왼쪽을 다 출력할 것이다
+from emp e1 left outer join emp e2 
 on e1.mgr=e2.empno;
 
 select e1.ename, e2.ename
-from emp e1 right outer join emp e2 -- 오른쪽을 다 출력할 것이다
+from emp e1 right outer join emp e2 
 on e1.empno=e2.mgr;
 
+
 select empno, ename, job, dname, loc
-from emp e join dept d -- 콤마는 cross join과 같다
+from emp e join dept d 
 on e.deptno=d.deptno
 where ename='SCOTT'
+;
+
+select empno, ename, job, dname, loc
+from emp e, dept d -- 콤마는 cross join과 같다
+where e.deptno=d.deptno
+    and e.ename='SCOTT' 
 ;
 
 select empno, ename, job, dname, loc
 from emp e cross join dept d
 ;
 
+-- salgrade 테이블을 참조해서 사원들의 사번, 이름, 급여, 급여 등급을 출력
 select empno, ename, losal, sal, hisal, grade
 from emp e join salgrade s
 on e.sal between s.losal and s.hisal
