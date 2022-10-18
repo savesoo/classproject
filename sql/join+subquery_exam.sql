a921cf95872ee3144e20efd4ea20b8f2b74c9cbe
soo <hsw2657@gmail.com>
2022-10-18 12:57:52 +0900

    edit
    진행부분까지

--- sql/join+subquery_exam.sql
+++ sql/join+subquery_exam.sql
@@ -3,27 +3,41 @@
 -- 1 마당서점의 고객이 요구하는 다음 질문에 대해 SQL 문을 작성하시오.
 
 -- (5) 박지성이 구매한 도서의 출판사 수
-select count(publisher)
+select count(distinct publisher)
 from book b, orders o
-where b.bookid=o.bookid and custid=1
+where b.bookid=o.bookid and custid = (select custid
+                                    from customer c
+                                    where name='박지성')
 ;
 
+select bookid
+from orders o
+where custid=1;
+
+select custid
+from customer c
+where name='박지성'
+;
 
 -- (6) 박지성이 구매한 도서의 이름, 가격, 정가와 판매가격의 차이
 select bookname, price, b.price-o.saleprice as pricegap
 from book b, orders o
-where o.bookid=b.bookid and custid=1
+where o.bookid=b.bookid and custid = (select custid
+                                    from customer c
+                                    where name='박지성')
 ;
 
--- (7) 박지성이 구매하지 않은 도서의 이름 ??????????
-select distinct bookname
-from book b, orders o
-where  b.bookid=o.bookid and o.custid!=1
+-- (7) 박지성이 구매하지 않은 도서의 이름
+select bookname
+from book b
+where bookid not in (select bookid
+                    from orders o, customer c
+                    where o.custid=c.custid and name='박지성')
 ;
 
-select distinct bookname
-from book b, (select bookid from orders o where b.bookid=o.bookid)
-where o.custid!=1
+select bookid
+from orders o, customer c
+where o.custid=c.custid and name='박지성'
 ;
 
 -- 2 마당서점의 운영자와 경영자가 요구하는 다음 질문에 대해 SQL 문을 작성하시오.
@@ -45,10 +59,21 @@ select sum(saleprice), avg(saleprice)
 from orders o
 ;
 
--- (10) 고객의 이름과 고객별 구매액 ???????????????
-select (select name from customer c where c.custid=o.custid), saleprice
+-- (10) 고객의 이름과 고객별 구매액
+select c.name, sum(o.saleprice)
+from orders o, customer c
+where o.custid = c.custid
+group by c.name
+;
+
+select sum(saleprice)
 from orders o
-where c.custid=o.custid
+group by custid
+;
+
+select name
+from orders o, customer c
+where o.custid = c.custid
 ;
 
 -- (11) 고객의 이름과 고객이 구매한 도서 목록
@@ -60,23 +85,77 @@ order by custid
 -- (12) 도서의 가격(Book 테이블)과 판매가격(Orders 테이블)의 차이가 가장 많은 주문
 select *
 from orders o, book b
-where in(b.bookid=o.bookid
+where b.bookid=o.bookid and b.price-o.saleprice = (select max(b.price-o.saleprice) as pricegap
+                                                    from orders o, book b
+                                                    where b.bookid=o.bookid)
+;
+
+-- 가장 큰 차이
+select max(b.price-o.saleprice) as pricegap
+from orders o, book b
+where b.bookid=o.bookid
+order by pricegap desc
 ;
 
 -- (select price from book b where b.bookid=o.bookid), (select saleprice from orders o where b.bookid=o.bookid)
 
 -- (13) 도서의 판매액 평균보다 자신의 구매액 평균이 더 높은 고객의 이름
-select (select name from customer c where avg(saleprice
-from orders o
-where avg(select saleprice from orders o where c.custid=o.custid) > avg(select saleprice from orders o where o.bookid=b.bookid)
+select c.name
+from customer c, 
+    (select custid, avg(saleprice) as avg_SP
+            from orders
+            group by custid) cusA
+where c.custid = cusA.custid
+and cusA.avg_SP > (select avg(saleprice)
+                   from orders)
 ;
+
+-- 도서의 판매액 평균
+select avg(saleprice)
+from orders
+;
+
+-- 고객별 자신의 구매액 평균
+select custid, avg(saleprice) as avg_SP
+from orders o
+group by custid
+;
+
 
 -- 3. 마당서점에서 다음의 심화된 질문에 대해 SQL 문을 작성하시오.
 
 -- (1) 박지성이 구매한 도서의 출판사와 같은 출판사에서 도서를 구매한 고객의 이름
-select (select name from customer c where custid=1)
-from orders o
-where bookid = any (select publisher from book b where b.bookid=o.bookid)
+select c.name
+from orders o, book b, customer c 
+where o.custid=c.custid and b.bookid=o.bookid 
+and publisher in (select publisher
+                from book b
+                where bookid in(select bookid from orders o, customer c where o.custid=c.custid and name='박지성'))
+and name<>'박지성'
 ;
 
+select bookname, publisher from book b, orders o where b.bookid=o.bookid;
+
+
+-- 박지성이 구매한 도서의 출판사
+select publisher
+from book b
+where bookid in(select bookid from orders o, customer c where o.custid=c.custid and name='박지성')
+order by publisher;
+
+-- 박지성이 구매한 도서 id
+select bookid
+from orders o, customer c 
+where o.custid=c.custid and name='박지성';
+
 -- (2) 두 개 이상의 서로 다른 출판사에서 도서를 구매한 고객의 이름
+select c.name
+from customer c, orders o, book b
+where publisher 
+;
+
+-- 두 개 이상의 서로 다른 출판사 도서
+select publisher 
+from book
+where bookid=(select bookid from book b, orders o where b.bookid=o.bookid
+;
