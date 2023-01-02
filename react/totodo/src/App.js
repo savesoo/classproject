2676ba93c345da6217e3950314b09c0f27da152b
savesoo <hsw2657@gmail.com>
2023-01-02 09:53:55 +0900

    react route - meterial UI
    .

---
+++ react/totodo/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import SamplePage from "./pages/SamplePage";
+
+function App() {
+  return (
+      <SamplePage />
+  );
+}
+
+export default App;
